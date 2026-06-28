17b2de5dfe60d9e2452e5d0000a656c67d24b7f5
Hai Liang Wang <hai@chatopera.com>
2026-06-28 15:48:11 +0800

    Add more codes for ch5

--- notebooks/practice_5_1.py
+++ notebooks/practice_5_1.py
@@ -1,108 +1,73 @@
+'''
+自动求导
+'''
+import numpy as np
 import torch
-import torch.optim as optimimzer
-import torch.nn as nn
-import sys
-
 torch.set_printoptions(edgeitems=2, linewidth=75)
 
-###########################
-# 给出位置单位的温度计的温度，让模型告诉我们，对应的摄氏温度是多少？
-###########################
-
-# X: 采集到的位置单位的温度
-t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
-
-# Y: 对应的摄氏度，目标计算的值
 t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]
-
+t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
 t_c = torch.tensor(t_c)
 t_u = torch.tensor(t_u) * 0.1
 
-###############################
-# 将数据集分为：train 训练/ validate 验证集(valid)
-###############################
 
-n_samples = t_u.shape[0]
-n_val = int(0.1 * n_samples)
-
-shuffled_indices = torch.randperm(n_samples)
-
-print("How many samples - ", n_samples)
-print("shuffled_indices", shuffled_indices)
-train_indices = shuffled_indices[:-n_val]
-vali_indices = shuffled_indices[-n_val:]
-
-train_data_x = t_u[train_indices].unsqueeze(1)
-train_data_y = t_c[train_indices]
-
-valid_data_x = t_u[vali_indices].unsqueeze(1)
-valid_data_y = t_c[vali_indices]
-
-print("Train indices", train_indices.numel())
-print("Validate indices", vali_indices.numel())
-
-model = nn.Linear(1, 1)
-
-# for param in model.parameters():
-#     print(param)
+def model(t_u, w, b):
+    return w * t_u + b
 
 
-def loss_fn(t_p, t_c):  # p --> predict
-    '''
-    损失函数，计算理想输出和实际输出的差异: 返回一个数值
-    t_p 和 t_c 都是向量，代表多个样本
-    '''
-    t_p = t_p.squeeze(dim=1)
-    # print("loss fn tp shape", t_p.shape)
-    # print("loss fn tc shape", t_c.shape)
-
-    squared_diffs = (t_p - t_c)**2  # 所有样本的损失的向量
-    return squared_diffs.mean()
+def loss_fn(t_p, t_c):
+    squared_diffs = (t_p - t_c)**2
+    return squared_diffs.mean()  # 返回值是一个张量
 
 
-# 开始训练，使用自动学习参数的机制：应用梯度求解变化率，多轮的更新
-learning_rate = 1e-3
-opt = optimimzer.Adam(model.parameters(), lr=learning_rate)
-epochs = 100000
+params = torch.tensor([1.0, 0.0], requires_grad=True)
+if params.grad is None:
+    print("Params is None after init.")
 
-for epoch in range(1, epochs + 1):
-    t_p = model(train_data_x)
-    loss = loss_fn(t_p=t_p, t_c=train_data_y)
 
-    opt.zero_grad()
+# loss = loss_fn(model(t_u, *params), t_c)
+# PyTorch 会发现 loss 的计算过程中，依赖了 params, 并且 params 的 requires_grad 设置上了: 那么，params 就会成为计算图中的一个节点，并非可以追踪回来，得到一个计算的数据公式
+# 这个数据公式，是由调用 backward 的张量，方向可以计算相对应的导数的。
+# loss.backward()  # 反向计算梯度
+
+'''
+自动求导
+https://zhuanlan.zhihu.com/p/2027423168176899509
+'''
+# params.grad
+
+
+'''
+利用自动求导，实现模型的参数评估
+'''
+
+epoch = 10000
+lr = 1e-2
+
+for i in range(epoch):
+    t_p = model(t_u, *params)
+
+    if params.grad is not None:
+        params.grad.zero_()
+    loss = loss_fn(t_p, t_c)
+    print("Epcho %s, loss %.5f" % (i, loss.item()))
+
     loss.backward()
 
-    # print("  w loss(%s), b loss(%s)" % (w.grad.item(), b.grad.item()))
-    opt.step()
+    with torch.no_grad():
+        params -= (lr) * params.grad
 
-    print('Epoch %d, Train Loss %f' % (epoch, float(loss)))
+print("Final result w: %s, b: %s" % (params[0].item(), params[1].item()))
 
-    if not torch.isfinite(loss).all():
-        break
-
-    if epoch % 100 == 0:
-        with torch.no_grad():
-
-            # print("model(valid_data_x)", model(valid_data_x).shape)
-            # print("valid_data_y", valid_data_y.shape)
-
-            valid_loss = loss_fn(model(valid_data_x), valid_data_y)
-            print('Epoch %d, Validate Loss %f' % (epoch, float(valid_loss)))
-
-print("Result:")
-
-for param in model.parameters():
-    print(param.item())
-
+'''
+Draw the line for fitting data
+'''
 from matplotlib import pyplot as plt
-
-t_p = model(t_u.unsqueeze(1))
+t_p = model(t_u, *params)  # <1>
 
 fig = plt.figure(dpi=600)
 plt.xlabel("Temperature (°Fahrenheit)")
 plt.ylabel("Temperature (°Celsius)")
 plt.plot(t_u.numpy(), t_p.detach().numpy())  # <2>
 plt.plot(t_u.numpy(), t_c.numpy(), 'o')
-plt.savefig("temp_unknown_plot.png", format="png")
-
-plt.show()
+plt.savefig("temp_unknown_plot.png", format="png")  # bookskip
