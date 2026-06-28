4bf5419cab4ddd68a01b3c2478729f9b651dda62
Hai Liang Wang <hai@chatopera.com>
2026-06-28 14:17:14 +0800

    Update practice_5_1.py

--- notebooks/practice_5.py
+++ notebooks/practice_5.py
@@ -11,10 +11,11 @@ torch.set_printoptions(edgeitems=2, linewidth=75)
 t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
 
 # Y: 对应的摄氏度，目标计算的值
-t_c = [0.5,  14.0, 15.0, 28.0, 11.0,  8.0,  3.0, -4.0,  6.0, 13.0, 21.0]
+t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]
 
 t_c = torch.tensor(t_c)
 t_u = torch.tensor(t_u)
+
 
 def model(x, w, b):
     '''
@@ -23,16 +24,16 @@ def model(x, w, b):
     return (w * x) + b
 
 
-def loss_fn(t_p, t_c): # p --> predict
+def loss_fn(t_p, t_c):  # p --> predict
     '''
     损失函数，计算理想输出和实际输出的差异: 返回一个数值
     t_p 和 t_c 都是向量，代表多个样本
     '''
-    squared_diffs = (t_p - t_c)**2 # 所有样本的损失的向量
+    squared_diffs = (t_p - t_c)**2  # 所有样本的损失的向量
     return squared_diffs.mean()
 
 
-w = torch.ones(()) # tensor: scalar(数值, 1.0)， vector ([1,2]), matrix([[1,2], [3,4]]), ....
+w = torch.ones(())  # tensor: scalar(数值, 1.0)， vector ([1,2]), matrix([[1,2], [3,4]]), ....
 b = torch.zeros(())
 
 t_p = model(t_u, w, b)
@@ -76,11 +77,14 @@ def dloss_fn(t_p, t_c):
 # dloss_fn 是关于loss 函数和输入 t_p 之间的导数，而我们需要的是 loss 和 (w,b) 之间的导数，
 # 而 t_p 和 w,b 之间是通过 model(w*x + b) 实现的，进而，通过链式法则：关于 dloss_fn, 和 model(w*x + b) 就可以求出 loss 和 (w,b) 之间的导数
 
+
 def dmodel_dw(t_u, w, b):
     return t_u
 
+
 def dmodel_db(t_u, w, b):
     return 1.0
+
 
 def grad_fn(t_u, t_c, t_p, w, b):
     dloss_dtp = dloss_fn(t_p, t_c)
@@ -92,7 +96,7 @@ def grad_fn(t_u, t_c, t_p, w, b):
 
 # 开始训练，使用自动学习参数的机制：应用梯度求解变化率，多轮的更新
 learning_rate = 1e-4
-epochs = 100
+epochs = 100000
 
 for epoch in range(1, epochs + 1):
     t_p = model(t_u, w, b)
@@ -103,7 +107,7 @@ for epoch in range(1, epochs + 1):
     w = w - (learning_rate * grad[0])
     b = b - (learning_rate * grad[1])
 
-    print('Epoch %d, Loss %f' % (epoch, float(loss))) # <3>
+    print('Epoch %d, Loss %f' % (epoch, float(loss)))  # <3>
 
     if not torch.isfinite(loss).all():
         break
@@ -119,10 +123,8 @@ t_p = model(t_u, w, b)  # <1>
 fig = plt.figure(dpi=600)
 plt.xlabel("Temperature (°Fahrenheit)")
 plt.ylabel("Temperature (°Celsius)")
-plt.plot(t_u.numpy(), t_p.detach().numpy()) # <2>
+plt.plot(t_u.numpy(), t_p.detach().numpy())  # <2>
 plt.plot(t_u.numpy(), t_c.numpy(), 'o')
-plt.savefig("temp_unknown_plot.png", format="png") 
+plt.savefig("temp_unknown_plot.png", format="png")
 
 plt.show()
-
-
--- notebooks/practice_5_1.py
+++ notebooks/practice_5_1.py
@@ -13,17 +13,17 @@ torch.set_printoptions(edgeitems=2, linewidth=75)
 t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
 
 # Y: 对应的摄氏度，目标计算的值
-t_c = [0.5,  14.0, 15.0, 28.0, 11.0,  8.0,  3.0, -4.0,  6.0, 13.0, 21.0]
+t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]
 
 t_c = torch.tensor(t_c)
-t_u = torch.tensor(t_u)
+t_u = torch.tensor(t_u) * 0.1
 
 ###############################
 # 将数据集分为：train 训练/ validate 验证集(valid)
 ###############################
 
 n_samples = t_u.shape[0]
-n_val = int(0.2 * n_samples)
+n_val = int(0.1 * n_samples)
 
 shuffled_indices = torch.randperm(n_samples)
 
@@ -38,24 +38,32 @@ train_data_y = t_c[train_indices]
 valid_data_x = t_u[vali_indices].unsqueeze(1)
 valid_data_y = t_c[vali_indices]
 
-print("Train indices", train_indices)
-print("Validate indices", vali_indices)
+print("Train indices", train_indices.numel())
+print("Validate indices", vali_indices.numel())
 
 model = nn.Linear(1, 1)
 
-def loss_fn(t_p, t_c): # p --> predict
+# for param in model.parameters():
+#     print(param)
+
+
+def loss_fn(t_p, t_c):  # p --> predict
     '''
     损失函数，计算理想输出和实际输出的差异: 返回一个数值
     t_p 和 t_c 都是向量，代表多个样本
     '''
-    squared_diffs = (t_p - t_c)**2 # 所有样本的损失的向量
+    t_p = t_p.squeeze(dim=1)
+    # print("loss fn tp shape", t_p.shape)
+    # print("loss fn tc shape", t_c.shape)
+
+    squared_diffs = (t_p - t_c)**2  # 所有样本的损失的向量
     return squared_diffs.mean()
 
 
 # 开始训练，使用自动学习参数的机制：应用梯度求解变化率，多轮的更新
-learning_rate = 1e-4
+learning_rate = 1e-3
 opt = optimimzer.Adam(model.parameters(), lr=learning_rate)
-epochs = 10000
+epochs = 100000
 
 for epoch in range(1, epochs + 1):
     t_p = model(train_data_x)
@@ -65,10 +73,7 @@ for epoch in range(1, epochs + 1):
     loss.backward()
 
     # print("  w loss(%s), b loss(%s)" % (w.grad.item(), b.grad.item()))
-
-    with torch.no_grad():
-        # 参数更新有两种写法
-        opt.step()
+    opt.step()
 
     print('Epoch %d, Train Loss %f' % (epoch, float(loss)))
 
@@ -78,14 +83,16 @@ for epoch in range(1, epochs + 1):
     if epoch % 100 == 0:
         with torch.no_grad():
 
-            print("model(valid_data_x)", model(valid_data_x).shape)
-            print("valid_data_y", valid_data_y.shape)
+            # print("model(valid_data_x)", model(valid_data_x).shape)
+            # print("valid_data_y", valid_data_y.shape)
 
             valid_loss = loss_fn(model(valid_data_x), valid_data_y)
             print('Epoch %d, Validate Loss %f' % (epoch, float(valid_loss)))
 
 print("Result:")
-print(model.parameters())
+
+for param in model.parameters():
+    print(param.item())
 
 from matplotlib import pyplot as plt
 
@@ -94,8 +101,8 @@ t_p = model(t_u.unsqueeze(1))
 fig = plt.figure(dpi=600)
 plt.xlabel("Temperature (°Fahrenheit)")
 plt.ylabel("Temperature (°Celsius)")
-plt.plot(t_u.numpy(), t_p.detach().numpy()) # <2>
+plt.plot(t_u.numpy(), t_p.detach().numpy())  # <2>
 plt.plot(t_u.numpy(), t_c.numpy(), 'o')
-plt.savefig("temp_unknown_plot.png", format="png") 
+plt.savefig("temp_unknown_plot.png", format="png")
 
 plt.show()
